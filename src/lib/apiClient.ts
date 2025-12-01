ebb4152c87e3cda94ee2e9136a55c505df21284a
imhidayatchitrali <hidayat.rehman64@gmail.com>
2025-12-01 13:53:57 +0500

    fixes

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'http://44.220.191.221/api';
+const API_BASE_URL = 'https://44.220.191.221/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
