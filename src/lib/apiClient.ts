a3f752bde8f79d573bc6bd8f3216dda334d92249
imhidayatchitrali <hidayat.rehman64@gmail.com>
2025-12-01 14:21:27 +0500

    change domain to https

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://44.220.191.221/api';
+// const API_BASE_URL = 'https://44.220.191.221/api';
+const API_BASE_URL = 'https://api.autowise.club/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
